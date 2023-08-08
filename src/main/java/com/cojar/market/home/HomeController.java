9efa2852fccb0c7da57b579bb13ace9ccf7bb18b
ohgil <ohgilhwan1957@gmail.com>
2023-08-08 09:38:30 +0900

    css

--- src/main/java/com/cojar/market/home/HomeController.java
+++ src/main/java/com/cojar/market/home/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String index() {
-        return "main";
+        return "home/main";
     }
 
 }
