db317d64d2f9f8fc6a104750b0715ab3ffa3ae21
ohgil <ohgilhwan1957@gmail.com>
2023-08-16 23:53:13 +0900

    í페이징 정렬 수정

--- src/main/java/com/project/market/product/service/ProductService.java
+++ src/main/java/com/project/market/product/service/ProductService.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +28,9 @@ public class ProductService {
     }
 
     public Page<Product> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 8);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 8, Sort.by(sorts));
         return this.productRepository.findAll(pageable);
     }
 
