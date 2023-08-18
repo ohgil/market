576c93459467fc140f9c4cbfcd63c434a4f32998
ohgil <ohgilhwan1957@gmail.com>
2023-08-18 11:47:45 +0900

    상품데이터 밀어 넣기

--- src/main/java/com/project/market/base/initData/DevInitData.java
+++ src/main/java/com/project/market/base/initData/DevInitData.java
@@ -1,6 +1,8 @@
 package com.project.market.base.initData;
 
 
+import com.project.market.member.service.MemberService;
+import com.project.market.product.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +12,15 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevInitData implements BeforeInitData {
     @Bean
-    CommandLineRunner initData() {
+    CommandLineRunner initData(MemberService memberService, ProductService productService) {
         return args -> {
             beforeInit();
+
+            String password = "{noop}1234";
+            memberService.join("user1", password, "user1@test.com", "user1");
+            memberService.join("user2", password, "user2@test.com", "user2");
+            memberService.join("user3", password, "user3@test.com", "user3");
+            memberService.join("user4", password, "user4@test.com", "user4");
         };
     }
 }
--- src/main/java/com/project/market/base/initData/TestInitData.java
+++ src/main/java/com/project/market/base/initData/TestInitData.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("tests")
+@Profile("test")
 public class TestInitData implements BeforeInitData {
     @Bean
     CommandLineRunner initData() {
